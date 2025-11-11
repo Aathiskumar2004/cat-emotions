a4d21c87db348a08133e1108733965ac1fd6dbd0
Aathiskumar2004 <aathiskumaraathis@gmail.com>
2025-11-11 22:11:57 +0530

    first commit

--- querySelector/index.js
+++
@@ -1,7 +0,0 @@
-// We can much more easily target pseudo elements like :checked
-const submitBtn = document.getElementById("submit-btn");
-
-submitBtn.addEventListener("click", function () {
-  const checkedRadio = document.querySelector('input[type="radio"]:checked');
-  console.log(checkedRadio.value);
-});
